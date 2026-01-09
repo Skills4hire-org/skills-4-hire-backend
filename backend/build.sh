a8102859f9f818e7ab9c1d1a2c51ce799402c317
ogennaisrael01 <ogennaisrael98@gmail.com>
2026-01-09 12:06:56 +0100

    feat: add redis to build

--- backend/build.sh
+++ backend/build.sh
@@ -2,30 +2,31 @@
 
 set -e
 
-echo "Initializing project..."
+# echo "Initializing project..."
 
-echo "Checking for model changes..."
-python3 manage.py makemigrations --noinput
+# echo "Checking for model changes..."
+# python3 manage.py makemigrations --noinput
 
-# 2. Apply migrations
-echo "Applying database migrations..."
-if python3 manage.py migrate --noinput; then
-    echo "SUCCESS: Migrations applied."
-else
-    echo "ERROR: Failed to apply migrations. Exiting."
-    exit 1
-fi
+# # 2. Apply migrations
+# echo "Applying database migrations..."
+# if python3 manage.py migrate --noinput; then
+#     echo "SUCCESS: Migrations applied."
+# else
+#     echo "ERROR: Failed to apply migrations. Exiting."
+#     exit 1
+# fi
 
-# Start celery worker and beat before running server
-echo Starting celery worker...
-celery -A config worker --loglevel=info &
+# # Start celery worker and beat before running server
+# echo Starting celery worker...
+# celery -A config worker --loglevel=info &
 
-echo Starting Celery Beat....
-celery -A config beat --loglevel=info &
-# 3. Start the server
-# Using exec ensures the Django process receives OS signals (like SIGTERM) directly
-echo "--> Starting development server on port 8000..."
-exec gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
+# echo Starting Celery Beat....
+# celery -A config beat --loglevel=info &
+# # 3. Start the server
+# # Using exec ensures the Django process receives OS signals (like SIGTERM) directly
+echo "--> Starting server..."
+
+exec gunicorn config.wsgi:application --workers 1 --threads 1 --timeout 120
 
 
 
