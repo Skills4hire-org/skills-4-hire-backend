a57343bc07028b307d9d31b80a44f4583f7ab795
ogennaisrael01 <ogennaisrael98@gmail.com>
2026-06-18 19:47:07 +0100

    feat(profiles): update ProviderProfileDetailSerializer to return the last three active comments and modify post retrieval to return a list

--- backend/orm_script.py
+++ backend/orm_script.py
@@ -12,16 +12,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE',  setting_module)
 django.setup()
 
 def run():
-    # from apps.chats.consumers import broadcast_chat_message
-    # from apps.chats.models import Message
-
-    # message = Message.objects.first()
-
-    # channel_layer = channels.layers.get_channel_layer()
-    # broadcast_chat_message(message=message)
-    
-    # return async_to_sync(channel_layer.receive)(f'chat_group_{message.conversation.conversation_id}_')
-
+    print("Run")
 
 if __name__ == "__main__":
     print(run())
