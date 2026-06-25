c5d7c450f78a2ddc931d5ba0e6958bebbc3a8974
ogennaisrael01 <ogennaisrael98@gmail.com>
2026-06-25 13:45:54 +0100

    set up google authentication

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -222,9 +222,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = "/var/www/myproject/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
